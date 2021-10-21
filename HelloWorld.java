930bc1e31677c94f7c495259be47158e880ec53c
root <root@ip-172-31-21-150.ec2.internal>
2021-10-21 08:55:16 +0000

    update java file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, My Dear World!"); 
+        System.out.println("Hello, My lovely World!"); 
     }
 }
 
